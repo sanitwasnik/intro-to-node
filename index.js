28d728b63d760abe7330a216bf4e724525a51f31
sanitwasnik <84314505+sanitwasnik@users.noreply.github.com>
2022-09-07 22:43:28 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const superheroes = require('superheroes');
+
+ var mySuperheroNAme=superheroes.random();
+//=> 'Spider-Ham'
+console.log(mySuperheroNAme);
+const supervillains = require('supervillains');
+var mysuperVillains= supervillains.random();
+console.log("vs");
+console.log(mysuperVillains);
